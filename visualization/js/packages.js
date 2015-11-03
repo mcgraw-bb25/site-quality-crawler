d4b3aaec6203437b96a865d5106e76a80e553f04
Mateusz Bocian <matbocian91@gmail.com>
2015-11-03 15:25:22 -0500

    Revert "Changing response model to use arbitrary ids instead of names"
    This reverts commit 004c774e40fe3e8bafeb1ca3c22501523fcf4bea.

--- visualization/js/packages.js
+++ visualization/js/packages.js
@@ -1,49 +1,48 @@
-(function () {
+(function() {
   packages = {
 
     // Lazily construct the package hierarchy from class names.
-    root: function (classes) {
+    root: function(classes) {
       var map = {};
 
-      function find(id, data) {
-        var node = map[id], i;
+      function find(name, data) {
+        var node = map[name], i;
         if (!node) {
-          node = map[id] = data || {id: id, children: []};
-          if (id.length) {
-            console.log(node);
-            node.parent = find(id.substring(0, i = id.lastIndexOf(".")));
+          node = map[name] = data || {name: name, children: []};
+          if (name.length) {
+            node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
             node.parent.children.push(node);
-            node.key = id.substring(i + 1);
+            node.key = name.substring(i + 1);
           }
         }
         return node;
       }
 
-      classes.forEach(function (d) {
+      classes.forEach(function(d) {
         find(d.name, d);
       });
 
       return map[""];
     },
 
-    // Return a list of links for the given array of nodes.
-    links: function (nodes) {
+    // Return a list of imports for the given array of nodes.
+    imports: function(nodes) {
       var map = {},
-          links = [];
+          imports = [];
 
       // Compute a map from name to node.
-      nodes.forEach(function (d) {
+      nodes.forEach(function(d) {
         map[d.name] = d;
       });
 
       // For each import, construct a link from the source to target node.
-      nodes.forEach(function (d) {
-        if (d.links) d.links.forEach(function (i) {
-          links.push({source: map[d.name], target: map[i]});
+      nodes.forEach(function(d) {
+        if (d.imports) d.imports.forEach(function(i) {
+          imports.push({source: map[d.name], target: map[i]});
         });
       });
 
-      return links;
+      return imports;
     }
 
   };
