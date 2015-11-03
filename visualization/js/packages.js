4bba94b0cb33cfeec19468c12b039b0a7f6534f9
Mateusz Bocian <matbocian91@gmail.com>
2015-11-03 15:50:17 -0500

    Fixed d3 "use arbitrary id" commit from before

--- visualization/js/packages.js
+++ visualization/js/packages.js
@@ -1,8 +1,8 @@
-(function() {
+(function () {
   packages = {
 
     // Lazily construct the package hierarchy from class names.
-    root: function(classes) {
+    root: function (classes) {
       var map = {};
 
       function find(name, data) {
@@ -10,15 +10,15 @@
         if (!node) {
           node = map[name] = data || {name: name, children: []};
           if (name.length) {
-            node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+            node.parent = find("");
             node.parent.children.push(node);
-            node.key = name.substring(i + 1);
+            node.key = name;
           }
         }
         return node;
       }
 
-      classes.forEach(function(d) {
+      classes.forEach(function (d) {
         find(d.name, d);
       });
 
@@ -26,18 +26,18 @@
     },
 
     // Return a list of imports for the given array of nodes.
-    imports: function(nodes) {
+    imports: function (nodes) {
       var map = {},
           imports = [];
 
       // Compute a map from name to node.
-      nodes.forEach(function(d) {
+      nodes.forEach(function (d) {
         map[d.name] = d;
       });
 
       // For each import, construct a link from the source to target node.
-      nodes.forEach(function(d) {
-        if (d.imports) d.imports.forEach(function(i) {
+      nodes.forEach(function (d) {
+        if (d.page_links) d.page_links.forEach(function (i) {
           imports.push({source: map[d.name], target: map[i]});
         });
       });
