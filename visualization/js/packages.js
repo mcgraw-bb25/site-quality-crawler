4ef2f8b3a457c37918b182594816257121f0b89f
Mateusz Bocian <matbocian91@gmail.com>
2015-11-03 16:04:13 -0500

    Fixed d3 "use arbitrary id" commits

--- visualization/js/packages.js
+++ visualization/js/packages.js
@@ -1,48 +1,48 @@
-(function() {
+(function () {
   packages = {
 
     // Lazily construct the package hierarchy from class names.
-    root: function(classes) {
+    root: function (classes) {
       var map = {};
 
-      function find(name, data) {
-        var node = map[name], i;
+      function find(id, data) {
+        var node = map[id], i;
         if (!node) {
-          node = map[name] = data || {name: name, children: []};
-          if (name.length) {
-            node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+          node = map[id] = data || {id: id, children: []};
+          if (id.length) {
+            node.parent = find("");
             node.parent.children.push(node);
-            node.key = name.substring(i + 1);
+            node.key = id;
           }
         }
         return node;
       }
 
-      classes.forEach(function(d) {
-        find(d.name, d);
+      classes.forEach(function (d) {
+        find(d.id, d);
       });
 
       return map[""];
     },
 
-    // Return a list of imports for the given array of nodes.
-    imports: function(nodes) {
+    // Return a list of page_links for the given array of nodes.
+    imports: function (nodes) {
       var map = {},
-          imports = [];
+          page_links = [];
 
-      // Compute a map from name to node.
-      nodes.forEach(function(d) {
-        map[d.name] = d;
+      // Compute a map from id to node.
+      nodes.forEach(function (d) {
+        map[d.id] = d;
       });
 
       // For each import, construct a link from the source to target node.
-      nodes.forEach(function(d) {
-        if (d.imports) d.imports.forEach(function(i) {
-          imports.push({source: map[d.name], target: map[i]});
+      nodes.forEach(function (d) {
+        if (d.page_links) d.page_links.forEach(function (i) {
+          page_links.push({source: map[d.id], target: map[i]});
         });
       });
 
-      return imports;
+      return page_links;
     }
 
   };
