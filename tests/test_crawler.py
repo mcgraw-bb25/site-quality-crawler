041c645efbd9a77a706463727a58aefabb988e85
matt mcgraw <matthew.mcgraw@gmail.com>
2015-11-09 14:32:38 -0500

    pep8 and pylinting test_crawler.py

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,15 +1,22 @@
 import unittest
 import json
 
-from crawler.crawler import Crawler
+# from crawler.crawler import Crawler
 from tests.mock_classes import MockCrawler
 
+
 class CrawlerUnitTest(unittest.TestCase):
+    '''
+    Tests the crawler with a MockCrawler object on local html
+    MockCrawler only over loads one function to allow mock
+    '''
+
     def test_001(self):
         ''' Tests mock crawler '''
         mock_crawler = MockCrawler(
             root_url='http://www.localtestsite.com/',
             start_url='http://www.localtestsite.com/mysite.html')
         mock_crawler.start_crawl()
-        print (json.dumps([pr.get_dictionary() for pr in mock_crawler.page_reports]))
-        self.assertEqual('a','a')
+        print (json.dumps([pr.get_dictionary() \
+                for pr in mock_crawler.page_reports]))
+        self.assertEqual('a', 'a')
