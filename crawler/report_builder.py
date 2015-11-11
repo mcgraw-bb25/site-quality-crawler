687509bbd4392514f2672904a1bbf4ff553d26ae
Mateusz Bocian <matbocian91@gmail.com>
2015-11-11 16:40:06 -0500

    Cleaning up comments, docstrings, and variable names

--- crawler/report_builder.py
+++ crawler/report_builder.py
@@ -6,68 +6,52 @@ from crawler.page_report import PageReport
 
 
 class ReportBuilder(PageReport):
-    '''
-    This takes in a set of PageReports
-    and builds a clean json report
-    '''
-
-    def __init__(self, pages):
-        self.pages = pages
-
-        # build_report method returns a string
-        self.report = ""
-
-    def clean_pageid(self):
-        '''
-        This method cleans the page ids in page report
-        so that page_links receives the hashes rather than the url
-
-        Invoked by start_crawl()
-        Probably requires a discussion with Mateusz sometime
-        '''
-
-        # initialize dictionary to hold url : id
-        page_set = {}
-
-        # run through page reports and append to page_set dic
-        for page in self.pages:
-            url_name = page.url
-            url_id = page.id
-            page_set[url_name] = url_id
-
-        # run through page reports again and swap page_links url with id
-        for page in self.pages:
-            position_counter = 0
-            for i in range(len(page.page_links)):
-                try:
-                    page_id = page_set[page.page_links[i]]
-                    page.page_links[i] = page_id
-                except:
-                    continue
-            position_counter = position_counter + 1
+    ''' Builds a JSON from a list of PageReports '''
+    def __init__(self, page_reports):
+        self.page_reports = page_reports
+        self.json_report = ""
 
     def build_report(self):
         ''' Method builds the report '''
-        # calls clean_pageid to prepare data to build report into
-        self.clean_pageid()
-
-        # calls Mateusz's function to publish json item for each page
-        self.report = json.dumps([page.get_dictionary() for page in self.pages])
-
+        self.replace_urls_with_ids()
+        pr_dictionary = [pr.get_dictionary() for pr in self.page_reports]
+        self.json_report = json.dumps(pr_dictionary, indent=4)
         self.save_report()
+
+    def replace_urls_with_ids(self):
+        '''
+        Replaces URLs in page reports page_links with
+        their respective PageReport ID
+        '''
+        # Build page report dictionary {url : id}
+        pr_set = {}
+        for pr in self.page_reports:
+            pr_set[pr.url] = pr.id
+
+        # Replace URLs in page_links with PageReport ID
+        for pr in self.page_reports:
+            for i in range(len(pr.page_links)):
+                try:
+                    page_id = pr_set[pr.page_links[i]]
+                    pr.page_links[i] = page_id
+                except KeyError:
+                    pr.page_links[i] = None
+            # The visualization will throw an error on unknown IDs
+            # We remove the key errors below
+            pr.page_links = list(filter((None), pr.page_links))
 
     def save_report(self):
         ''' Method to implement saving report '''
-
-        timestamp = str(datetime.datetime.now().year) \
-            + "_" + str(datetime.datetime.now().month) \
-            + "_" + str(datetime.datetime.now().day) \
-            + "_" + str(datetime.datetime.now().minute) \
-            + "_" + str(datetime.datetime.now().second)
+        now = datetime.datetime.now()
+        timestamp_string = str(now.year) \
+            + "_" + str(now.month) \
+            + "_" + str(now.day) \
+            + "_" + str(now.minute) \
+            + "_" + str(now.second)
 
         curpath = os.getcwd()
         newpath = curpath + '/reports/'
-        newfile = newpath + 'site_report' + timestamp + '.json'
+        newfile = newpath + 'site_report-' + timestamp_string + '.json'
 
         with open(newfile, 'w') as reportfile:
-            reportfile.write(self.report)
+            reportfile.write(self.json_report)
