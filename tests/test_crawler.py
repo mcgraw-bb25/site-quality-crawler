a4990c1a1451de7b2f986c6384462a6141e729ff
matt mcgraw <matthew.mcgraw@gmail.com>
2015-11-11 13:07:28 -0500

    changes to test_crawler to test full crawler also tests report_builder

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -35,3 +35,40 @@ class CrawlerUnitTest(unittest.TestCase):
             mockjson.write(mock_json_data)
 
         # print (mock_json_data)
+
+    def test_002(self):
+        ''' Tests output of mock crawler '''
+
+        # initialize test report file output
+        curpath = os.getcwd()
+        newpath = curpath + '/reports/'
+        newfile = newpath + 'mock_site_report.json'
+
+        with open(newfile, 'r') as mockjsonreport:
+            mock_json_report = mockjsonreport.read()
+
+        # print (mock_json_report)
+        self.assertIsNotNone(mock_json_report)
+        self.assertIn('www.localtestsite.com/mysite.html', mock_json_report)
+        self.assertIn('www.localtestsite.com/aboutus.html', mock_json_report)
+        self.assertIn('www.localtestsite.com/login.html', mock_json_report)
+
+        '''
+        This entire section doesn't work because the dict builder
+        produces different json files on each run.
+
+        Should discuss with Mateusz
+
+        # initialize known test output
+        curpath = os.getcwd()
+        newpath = curpath + '/tests/'
+        newfile = newpath + 'mock_test_output.json'
+
+        with open(newfile, 'r') as knownjsonreport:
+            known_json_report = knownjsonreport.read()
+
+        print (known_json_report)
+        self.assertIsNotNone(known_json_report)
+
+        self.assertEqual(mock_json_report, known_json_report)
+        '''
