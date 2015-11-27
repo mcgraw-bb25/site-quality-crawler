688b1fa860c2bce5bd0898ce51f9826263d7cc03
matt mcgraw <matthew.mcgraw@gmail.com>
2015-11-27 17:55:48 -0500

    modified server and others to support new index

--- server.py
+++ server.py
@@ -1,18 +1,30 @@
+import os
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
+from app.get_report_list import get_report_list
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    reports = get_report_list()
+    return render_template('index.html', reports=reports)
 
 
-@app.route("/site_report.json")
-def polls_json():
-    report_json = "reports/site_report.json"
-    with open(report_json, 'r') as report_file:
-        return report_file.read()
+@app.route('/site_report/<site_report_file>')
+def site_report(site_report_file):
+    report_filename = '/reports/' + site_report_file
+    return render_template('site_report.html', site_report_file=report_filename)
+
+
+@app.route('/reports/<path:filename>')
+def send_json(filename):
+    curpath = os.getcwd()
+    newpath = curpath + '/reports/'
+    return send_from_directory(newpath, filename)
 
 
 if __name__ == "__main__":
